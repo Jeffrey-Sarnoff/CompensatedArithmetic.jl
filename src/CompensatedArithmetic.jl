190f1526b86e53707dd56f8bfeff88567506431a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-03-19 09:23:21 -0400

    Update CompensatedArithmetic.jl

--- src/CompensatedArithmetic.jl
+++ src/CompensatedArithmetic.jl
@@ -1,10 +1,11 @@
 module CompensatedArithmetic
 
-export csdDot, csdDet2x2, csdCross3D
+export csdDot, csdDet2x2, csdCross3D, csdHorner
 
 using ErrorfreeArithmetic
 
 include("arith.jl")
 include("det.jl")
+include("horner.jl")
 
 end # module
